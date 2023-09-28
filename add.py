5afad692ef0fc024f9c82ceec87e7c7dae896122
Your Name <you@example.com>
2023-09-27 17:35:02 -0700

    my

---
+++ add.py
@@ -0,0 +1,17 @@
+#class
+""" 
+userdefine datatype,collection of variables , objects ,function and methods."""
+ #example:- add two no using class.
+#using class keyword
+
+class Add:
+	def getdata( self, x, y):
+		self.a=X
+		self.b=y
+	def putdata(self):
+		print("a=",self.a)
+		print("b=",self.b)
+#___main____
+k=Add()
+k.getdata(5688456789,0987654321)
+k.putdata()
